a2fcd649c9a25abbb5e02815b6a8718b101c228c
Duowriters <contact.duowriters@gmail.com>
2026-06-23 09:59:29 +0530

    Updated LinkedIn Link2

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -53,7 +53,7 @@ export default function Footer() {
   
   <div className="space-y-2">
     <a
-      href="https://www.linkedin.com/in/duowriters-undefined-799b29418?utm_source=share_via&utm_content=profile&utm_medium=member_android"
+      href="https://www.linkedin.com/in/duo-writers-799b29418?utm_source=share_via&utm_content=profile&utm_medium=member_android"
       className="block opacity-70 hover:opacity-100 transition"
     >
       LinkedIn
